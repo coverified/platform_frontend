c60fe729c6c551c21e9e19d9592bd6603f36a727
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2020-07-23 16:33:17 +0200

    add option to override api endpoint

--- src/common.js
+++ src/common.js
@@ -2,7 +2,7 @@ import ApolloClient, {gql} from 'apollo-boost';
 
 export const env = window.location.hostname.endsWith('.docker') || window.location.hostname.endsWith('localhost') ? 'development' : 'production';
 
-export const apiEndpionts = {
+export const apiEndpionts = window.__coverified_overrride_api_endpoints || {
     development: 'http://coverified-backend-keystone.docker/admin/api',
     production: 'https://gql.coverified.info/admin/api',
 }
